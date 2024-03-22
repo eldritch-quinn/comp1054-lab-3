6d359eb60e5fd31934468ec46073d8bc55a8ddf1
eldritch-quinn <quinn.faulkner02@gmail.com>
2024-03-22 10:42:30 -0400

    added comments

--- js/lab3.js
+++ js/lab3.js
@@ -2,19 +2,30 @@
 const tabpanels = document.querySelectorAll(`div[role="tabpanel"]`)
 const tabs = document.querySelectorAll(`.tabs ul li a`)
 
-
-
+// define a click handler function
 function handler() {
+
+    // for each tab panel..
     tabpanels.forEach(panel => {
+        // get href from click and remove hashtag
         let href = this.getAttribute("href").substring(1);
         
+        // if the href matches the panel id..
         if (href != panel.getAttribute("id"))
+
+            // hide panel
             panel.style.display = "none";
+        
+        // otherwise..
         else 
+            // show panel
             panel.style.display = "block";
     })
 }
 
+// for each tab..
 tabs.forEach(tab => {
+    
+    // Add the handler function on click
     tab.addEventListener('click', handler)
 });
